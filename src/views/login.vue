0e343fc3e8ab91e3a25c580816cafd7279198aeb
tarro55 <hammee196@gmail.com>
2018-12-15 23:39:56 +0700

    ทำฟอร์มหน้า login

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,14 +1,15 @@
 <!----------Make By YourName---------------->
- <template>
-<div >
-    <h1 id="title" >log in</h1>
+ <template >
+<div class="box-brown " >
+    <form>
+    <v-icon id="icon" class=" wh" style="font-size:130px; border-radius:50%;"> person </v-icon>
     <br>
-    <el-input id="user" placeholder="Username" v-model="Username"  ></el-input>
+    <el-input id="user" type="text" placeholder="Username" v-model="Username"  ></el-input>
     <br><br>
-    <el-input id="password"  placeholder="Password" v-model="Password"></el-input>
+    <el-input id="password" type="password" placeholder="Password" v-model="Password"></el-input>
     <br><br>
     <el-row >
-        <el-button id="login" type="warning" round @click="$router.push('/index')">Login</el-button>
+        <el-button  id="login" type="submit"  @click="$router.push('/index')" round>Login</el-button>
     </el-row>
     <br>
       <!--<p><a @click="$router.push('/test')">test</a></p> &nbsp -->
@@ -17,6 +18,7 @@
     <p><a id="forget" @click="$router.push('/forgetPass')">Foget Password</a></p> 
     <br><br>
     <p id="signin">Don't have account ? <a @click="$router.push('/type')">sign up</a></p>
+    </form>
     </div>
 </template>
 
@@ -35,8 +37,7 @@ props:{
     /*-------------------------DataVarible---------------------------------------*/
     data() {
     return {
-        Username:'',
-        Password:'',
+        from:{},
         };
     }, 
     /*-------------------------Run Methods when Start this Page------------------------------------------*/
@@ -61,15 +62,15 @@ methods:{
     }
 </script>
 <style>
-#title{
-    margin: 110px 50px 50px 135px;
+#icon{
+    margin: 90px 50px 50px 120px;
 }
 
 #password{
     margin-top: 20px;
 }
 #login{
-    margin: 20px 50px 3px 135px;
+    margin: 20px 50px 3px 150px;
 }
 #forget{
     margin-left: 230px 
